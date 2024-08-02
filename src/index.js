cd54780ca3139b395caf2459c09a08dbfbd6a82c
Jena Stewart <66694416+jenastewart@users.noreply.github.com>
2024-08-01 19:14:09 -0700

    Added basic city search function.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchFormElement = document.querySelector(".search-form");
+searchFormElement.addEventListener("submit", search);
