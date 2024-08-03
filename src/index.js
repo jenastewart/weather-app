aaef2db1715ce3e4f69dad31d4cb5c044ea49d17
Jena Stewart <66694416+jenastewart@users.noreply.github.com>
2024-08-02 17:10:16 -0700

    Created function to update current temperature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+function updateWeatherData(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#current-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "a17bt048aac153ed9acb6efaf1a6aobf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
+
+//create function to get units (imperial vs metric) for query
+
 let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", search);
+
+searchCity("Portland");
